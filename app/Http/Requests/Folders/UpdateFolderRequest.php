2652a8211a4b337b22c69809efa608784dbaeacb
khanlarazizov <xanlar.ezizov18@gmail.com>
2023-12-04 15:58:19 +0400

    folder-request-added

---
+++ app/Http/Requests/Folders/UpdateFolderRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Folders;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateFolderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|min:3|max:20'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Ad daxil edin.'
+        ];
+    }
+
+}
