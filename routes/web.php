2c079f4de25a960e9dbbfaf2ef47b87775fbea76
khanlarazizov <xanlar.ezizov18@gmail.com>
2023-12-04 16:00:17 +0400

    resource-route-added

--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\contractController;
-use App\Http\Controllers\protocolController;
-use App\Http\Controllers\folderController;
+use App\Http\Controllers\ContractController;
+use App\Http\Controllers\ProtocolController;
+use App\Http\Controllers\FolderController;
 
-Route::prefix('contract')->controller(contractController::class)->group(function (){
-    Route::get('index','index')->name('contract.index');
+Route::resource('contracts', ContractController::class)->except(['show']);
+Route::get('contracts/{contracts}/download',[ContractController::class, 'download'])->name('contracts.download');
 
-    Route::get('create','create')->name('contract.create');
-    Route::post('store','store')->name('contract.store');
+Route::resource('protocols', ProtocolController::class)->except(['show']);
+Route::get('protocols/{id}/download',[ProtocolController::class, 'download'])->name('protocols.download');
 
-    Route::get('edit/{id}','edit')->name('contract.edit');
-    Route::put('update/{id}','update')->name('contract.update');
-
-    Route::delete('delete/{id}','delete')->name('contract.delete');
-
-    Route::get('download/{id}','download')->name('contract.download');
-});
-
-Route::prefix('protocol')->controller(protocolController::class)->group(function (){
-    Route::get('index','index')->name('protocol.index');
-
-    Route::get('create','create')->name('protocol.create');
-    Route::post('store','store')->name('protocol.store');
-
-    Route::get('edit/{id}','edit')->name('protocol.edit');
-    Route::put('update/{id}','update')->name('protocol.update');
-
-    Route::delete('delete/{id}','delete')->name('protocol.delete');
-
-    Route::get('download/{id}','download')->name('protocol.download');
-});
+Route::resource('folders', FolderController::class)->except(['show','update']);
+Route::post('folders/{folder}',[FolderController::class, 'update'])->name('folders.update');//put la yollaya bilmədim hələ ki
 
 
-Route::prefix('folder')->controller(folderController::class)->group(function (){
-    Route::get('index','index')->name('folder.index');
-    Route::get('index/{id:folders}','folder')->name('folder');
 
-    Route::get('create','create')->name('folder.create');
-    Route::post('store','store')->name('folder.store');
-
-    Route::get('edit/{id}','edit')->name('folder.edit');
-    Route::post('update/{id}','update')->name('folder.update');
-
-    Route::delete('delete/{id}','delete')->name('folder.delete');
-});
+//Route::prefix('folder')->controller(FolderController::class)->group(function (){
+//    Route::get('index','index')->name('folder.index');
+//    Route::get('index/{id:folders}','folder')->name('folder');
+//
+//    Route::get('create','create')->name('folder.create');
+//    Route::post('store','store')->name('folder.store');
+//
+//    Route::get('edit/{id}','edit')->name('folder.edit');
+//    Route::post('update/{id}','update')->name('folder.update');
+//
+//    Route::delete('delete/{id}','delete')->name('folder.delete');
+//});
