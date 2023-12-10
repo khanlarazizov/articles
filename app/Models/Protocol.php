3dcb2866206b36f9ae0df4d376b9ca2ddb24efae
khanlarazizov <xanlar.ezizov18@gmail.com>
2023-12-11 00:49:23 +0400

    relationship-changed

--- app/Models/Protocol.php
+++ app/Models/Protocol.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Protocol extends Model
 {
@@ -23,9 +22,9 @@ class Protocol extends Model
         ];
     }
 
-    public function contract():HasOne
+    public function contract():BelongsTo
     {
-        return $this->hasOne(Contract::class,"id","contract_id");
+        return $this->belongsTo(Contract::class);
     }
 
     public function scopeName($query,$name){
