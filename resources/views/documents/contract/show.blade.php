8d5fd929e8b26a902a9ade1a86108437943ada61
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-01-22 01:29:00 +0400

    show-modal-changed

--- resources/views/documents/contract/show.blade.php
+++ resources/views/documents/contract/show.blade.php
@@ -2,11 +2,10 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <h1 class="modal-title fs-5" id="contract_title">Modal title</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <p><strong>ID:</strong> <span id="contract_id"></span></p>
                 <p><strong>Ad:</strong> <span id="contract_name"></span></p>
                 <p><strong>Tarix:</strong> <span id="contract_date"></span></p>
                 <p><strong>Bağlı olduğu qovluq:</strong> <span id="contract_folder_id"></span></p>
--- resources/views/documents/protocol/show.blade.php
+++ resources/views/documents/protocol/show.blade.php
@@ -3,11 +3,10 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <h1 class="modal-title fs-5" id="protocol_title">Modal title</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <p><strong>ID:</strong> <span id="protocol_id"></span></p>
                 <p><strong>Ad:</strong> <span id="protocol_name"></span></p>
                 <p><strong>Tarix:</strong> <span id="protocol_date"></span></p>
                 <p><strong>Bağlı olduğu Müqavilə:</strong> <span id="protocol_contract_id"></span></p>
